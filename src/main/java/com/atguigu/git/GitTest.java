bbd394f7bcc8d0a84b9e15a5cbe78192589ee374
qiaozhongli <592300543@qq.com>
2022-10-06 10:44:52 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author QIAO
+ * @function
+ * @create 2022-10-06 10:36
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
