c798d8f05e717f404e745d71c51dc4180ae7a046
qiaozhongli <592300543@qq.com>
2022-10-11 14:19:28 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,5 +13,7 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("pull git");
         System.out.println("push git");
+        System.out.println("gitee git");
+        System.out.println("gitee1 git");
     }
 }
